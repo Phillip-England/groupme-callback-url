23989b8071010684ae114405c224be5dc24eceae
Phillip-England <Phillip.mark.england@gmail.com>
2023-03-28 20:37:09 -0500

    testing on railway

--- src/routes/create_routes.rs
+++ src/routes/create_routes.rs
@@ -1,19 +1,21 @@
-use axum::{http::HeaderValue, http::Method, routing::get, Router};
+use axum::{
+  http::Method,
+  routing::post, 
+  Router
+};
 use mongodb::Client;
 use tower_http::cors::{Any, CorsLayer};
 
 pub async fn create_routes(client: Client) -> Router {
+
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PUT])
         .allow_origin(Any);
 
     return Router::new()
-        .route(
-            "/",
-            get(|| async {
-                println!("HIT");
-                "Hello, world!"
-            }),
-        )
-        .layer(cors);
+    .route("/", post(|| async { 
+      println!("HIT");
+      "Hello, world!" 
+    }))
+    .layer(cors)
 }
